c81f26f5485517d4874c28370620cb8e757dc9f0
Umidjan <najdimu@gmail.com>
2025-02-05 15:37:48 +0500

    during choose tariff

--- app/src/main/java/com/example/setwallpaper/AddWallpaperFragment.kt
+++ app/src/main/java/com/example/setwallpaper/AddWallpaperFragment.kt
@@ -23,6 +23,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.setwallpaper.databinding.FragmentAddWallpaperBinding
 import kotlinx.coroutines.Dispatchers
@@ -46,7 +48,7 @@ class AddWallpaperFragment : Fragment(R.layout.fragment_add_wallpaper) {
 
     private lateinit var binding: FragmentAddWallpaperBinding
 
-    private var colorId = -1
+    private var tariffID = -1
     private var bitmapWallpaper: Bitmap? = null
 
 
@@ -60,223 +62,102 @@ class AddWallpaperFragment : Fragment(R.layout.fragment_add_wallpaper) {
                 .commit()
         }
 
-        binding.wallpaperImageView.setOnClickListener {
-            pickImageFromGallery()
-        }
+        binding.layoutTariffWeek.setOnClickListener { checkTariff(1) }
+        binding.layoutTariffMonth.setOnClickListener { checkTariff(2) }
+        binding.layoutTariff3Month.setOnClickListener { checkTariff(3) }
+        binding.layoutTariff6Month.setOnClickListener { checkTariff(4) }
+        binding.layoutTariffYear.setOnClickListener { checkTariff(5) }
 
-        // Dynamically create RadioButtons
-        colors.forEachIndexed { index, colorHex ->
-            val radioButton = RadioButton(requireContext()).apply {
-                id = index // Unique ID for each button
-                layoutParams = RadioGroup.LayoutParams(70, 70).apply {
-                    setMargins(16, 0, 16, 0)
-                }
-                buttonDrawable = null // Remove default RadioButton drawable
-                background = createUnselectedColor(colorHex) // Apply color selector
-            }
-
-            binding.btnRadioGroup.addView(radioButton)
-        }
-
-        // Handle selection
-        binding.btnRadioGroup.setOnCheckedChangeListener { _, checkedId ->
-            val selectedColor = colors[checkedId]
-                colorId = checkedId
-            binding.chooseColorTitle.setTextColor(android.graphics.Color.parseColor(selectedColor))
-            updateButtonBackground(binding.btnRadioGroup, checkedId)
-        }
-
-        val items = listOf(getString(R.string.choose_spinner_option), "Telegram", "WhatsApp", "Email")
+        TODO("tariflaring subtext gorip chikmali" +
+                "check box true bolmasa error text chikmali" +
+                "activity ishlamali tarif saylamak, cheking, btn buy" +
+                "birinji page ishlamali")
 
 
-        val adapter = object: ArrayAdapter<String>(requireContext(), R.layout.layout, items){
-            override fun isEnabled(position: Int): Boolean {
-                return position != 0
-            }
-
-            override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
-                val view12 = super.getDropDownView(position, convertView, parent)
-                val textView = view12 as TextView
-
-                // Set the color of the disabled item
-                if (position == 0) {
-                    textView.setTextColor(resources.getColor(R.color.rang4, null))
-                } else {
-                    textView.setTextColor(resources.getColor(R.color.white1, null))
-                }
-                return view12
-            }
-        }
-
-        binding.spinner.adapter = adapter
-
-        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                when (p2){
-                    1->{
-                        binding.howContactEditText.visibility = View.VISIBLE
-                        binding.howContactEditText.hint = "username"
-                        binding.howContactEditText.inputType = android.text.InputType.TYPE_CLASS_TEXT
-                        binding.howContactEditText.text.clear()
-                    }
-                    2->{
-                        binding.howContactEditText.visibility = View.VISIBLE
-                        binding.howContactEditText.hint = "+888 00 000 000 0"
-                        binding.howContactEditText.inputType = android.text.InputType.TYPE_CLASS_TEXT or android.text.InputType.TYPE_CLASS_PHONE
-                        binding.howContactEditText.text.clear()
-                    }
-                    3->{
-                        binding.howContactEditText.visibility = View.VISIBLE
-                        binding.howContactEditText.hint = "example@ex.com"
-                        binding.howContactEditText.inputType = android.text.InputType.TYPE_CLASS_TEXT or android.text.InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
-                        binding.howContactEditText.text.clear()
-                    }
-                }
-
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-        }
 
 
-        binding.btnSendWallpaper.setOnClickListener {
-            val wallpaperName = binding.contentName.text.toString()
-            val designerName = binding.designerName.text.toString()
-            val wallpaperSize = binding.contentSize.text.toString()
-            val userContact = binding.howContactEditText.text.toString()
 
-            if (wallpaperName.isEmpty() || wallpaperSize.isEmpty() || userContact.isEmpty()
-                || colorId == -1 || bitmapWallpaper == null) {
-                Toast.makeText(requireContext(),R.string.please_fill, Toast.LENGTH_SHORT).show()
-            }
-            else {
-                try{
-                    val bitmapByteArray = bitmapsToListByteArray(bitmapWallpaper)
-                    val jsonObject = addTextsToJsonObject(wallpaperName, designerName, wallpaperSize, userContact, colorsName[colorId])
-                    val jsonByteArray = jsonToByteArray(jsonObject)
-                    val zipFile = createZipInMemory(jsonByteArray, bitmapByteArray)
-                    sendToServerZip(zipFile, "https://yourserver.com/api/upload/theme")
-                    binding.progressBar.visibility = View.VISIBLE
-                    binding.btnSendWallpaper.text = ""
-                    binding.btnSendWallpaper.isEnabled = false
-
-                }catch  (e: Exception) {
-                    buttonClickTrue()
-                    Toast.makeText(requireContext(), e.message, Toast.LENGTH_SHORT).show()
-                    e.printStackTrace()
-                }
-            }
-        }
+//        binding.btnSendWallpaper.setOnClickListener {
+//            val wallpaperName = binding.contentName.text.toString()
+//            val designerName = binding.designerName.text.toString()
+//            val wallpaperSize = binding.contentSize.text.toString()
+//            val userContact = binding.howContactEditText.text.toString()
+//
+//            if (wallpaperName.isEmpty() || wallpaperSize.isEmpty() || userContact.isEmpty()
+//                || colorId == -1 || bitmapWallpaper == null) {
+//                Toast.makeText(requireContext(),R.string.please_fill, Toast.LENGTH_SHORT).show()
+//            }
+//            else {
+//                try{
+//                    val bitmapByteArray = bitmapsToListByteArray(bitmapWallpaper)
+//                    val jsonObject = addTextsToJsonObject(wallpaperName, designerName, wallpaperSize, userContact, colorsName[colorId])
+//                    val jsonByteArray = jsonToByteArray(jsonObject)
+//                    val zipFile = createZipInMemory(jsonByteArray, bitmapByteArray)
+//                    sendToServerZip(zipFile, "https://yourserver.com/api/upload/theme")
+//                    binding.progressBar.visibility = View.VISIBLE
+//                    binding.btnSendWallpaper.text = ""
+//                    binding.btnSendWallpaper.isEnabled = false
+//
+//                }catch  (e: Exception) {
+//                    buttonClickTrue()
+//                    Toast.makeText(requireContext(), e.message, Toast.LENGTH_SHORT).show()
+//                    e.printStackTrace()
+//                }
+//            }
+//        }
 
 
     }
 
-    private fun pickImageFromGallery() {
-        val pickImageIntent = Intent(Intent.ACTION_PICK).apply {
-            type = "image/*"
-        }
-        galleryLauncher.launch(pickImageIntent)
-    }
 
-    // Handle the result from the gallery
-    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
-    { result ->
-        if (result.resultCode == AppCompatActivity.RESULT_OK) {
-            val imageUri: Uri? = result.data?.data
-            imageUri?.let { uri -> pictureToImageView(uri) }
+    private fun checkTariff(layout: Int){
+        when (layout){
+            1 ->{
+                tariffID = 1
+                binding.layoutTariffWeek.background = ContextCompat.getDrawable(requireContext(), R.drawable.selected_tariff)
+                binding.layoutTariffMonth.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariff3Month.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariff6Month.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariffYear.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
             }
-        }
-
-
-    private fun pictureToImageView(uri: Uri){
-        try {
-            val inputStream: InputStream? = requireActivity().contentResolver.openInputStream(uri)
-            val bitmap: Bitmap = BitmapFactory.decodeStream(inputStream)
-            val resizeBitmap = resizeBitmapWidth(bitmap, 720)
-            binding.wallpaperImageView.setImageBitmap(resizeBitmap)
-            binding.wallpaperTextView2.visibility = View.GONE
-            bitmapWallpaper = bitmap
-            // Notify user (optional)
-            Toast.makeText(requireContext(), R.string.success, Toast.LENGTH_SHORT).show()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Toast.makeText(requireContext(), R.string.error, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    // Define a list of colors
-    private val colors = listOf(
-        "#FFFFFF", // White
-        "#FF0000", // Red
-        "#0000FF", // Blue
-        "#000000", // Black
-        "#00FF00", // Green
-        "#FFFF00", // Yellow
-        "#FFA500", // Orange
-        "#800080", // Purple
-        "#00FFFF", // Cyan
-        "#FB6391", // Pink
-        "#A52A2A", // Brown
-        "#808080"  // Gray
-    )
-
-    private val colorsName = listOf(
-        "White", // White
-        "Red", // Red
-        "Blue", // Blue
-        "Black", // Black
-        "Green", // Green
-        "Yellow", // Yellow
-        "Orange", // Orange
-        "Purple", // Purple
-        "Cyan", // Cyan
-        "Pink", // Pink
-        "Brown", // Brown
-        "Gray"  // Gray
-    )
-
-    private fun createColorSelector(colorHex: String): GradientDrawable {
-        return GradientDrawable().apply {
-            shape = GradientDrawable.OVAL
-            setColor(android.graphics.Color.parseColor(colorHex)) // Set the fill color
-            setStroke(4, android.graphics.Color.BLACK) // Add a stroke
-        }
-    }
-
-    private fun createUnselectedColor(colorHex: String): GradientDrawable {
-        return GradientDrawable().apply {
-            shape = GradientDrawable.OVAL
-            setColor(android.graphics.Color.parseColor(colorHex)) // Set the fill color for unselected
-            setStroke(2, android.graphics.Color.WHITE) // Thin stroke for unselected state
-        }
-    }
-
-    private fun updateButtonBackground(radioGroup: RadioGroup, checkedId: Int) {
-        for (i in 0 until radioGroup.childCount) {
-            val radioButton = radioGroup.getChildAt(i) as RadioButton
-            val colorHex = colors[i]
-
-            // Update background for each RadioButton (selected or unselected)
-            if (i == checkedId) {
-                radioButton.background = createColorSelector(colorHex) // Selected color
-            } else {
-                radioButton.background = createUnselectedColor(colorHex) // Unselected color
+            2 ->{
+                tariffID = 2
+                binding.layoutTariffWeek.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariffMonth.background = ContextCompat.getDrawable(requireContext(), R.drawable.selected_tariff)
+                binding.layoutTariff3Month.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariff6Month.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariffYear.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+            }
+            3 ->{
+                tariffID = 3
+                binding.layoutTariffWeek.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariffMonth.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariff3Month.background = ContextCompat.getDrawable(requireContext(), R.drawable.selected_tariff)
+                binding.layoutTariff6Month.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariffYear.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+            }
+            4 ->{
+                tariffID = 4
+                binding.layoutTariffWeek.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariffMonth.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariff3Month.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariff6Month.background = ContextCompat.getDrawable(requireContext(), R.drawable.selected_tariff)
+                binding.layoutTariffYear.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+            }
+            5 ->{
+                tariffID = 5
+                binding.layoutTariffWeek.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariffMonth.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariff3Month.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariff6Month.background = ContextCompat.getDrawable(requireContext(), R.drawable.unselected_tariff)
+                binding.layoutTariffYear.background = ContextCompat.getDrawable(requireContext(), R.drawable.selected_tariff)
             }
         }
     }
 
 
 
-    private fun resizeBitmapWidth(bitmap: Bitmap, newWidth: Int): Bitmap {
-        // Calculate the new height to maintain the aspect ratio
-        val aspectRatio = bitmap.height.toFloat() / bitmap.width
-        val newHeight = (newWidth * aspectRatio).toInt()
 
-        // Scale the bitmap
-        return Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true)
-    }
 
     private fun bitmapsToListByteArray(bitmap: Bitmap?, format: Bitmap.CompressFormat =
         Bitmap.CompressFormat.JPEG, quality: Int = 75): ByteArray {
@@ -347,13 +228,13 @@ class AddWallpaperFragment : Fragment(R.layout.fragment_add_wallpaper) {
                         .commit()
 
                 }.onFailure {
-                    buttonClickTrue()
+                  //  buttonClickTrue()
                     Toast.makeText(requireContext(), R.string.error_response_message, Toast.LENGTH_SHORT).show()
                     println("Upload failed: ${it.message}")
                 }
 
             } catch (e: Exception) {
-                buttonClickTrue()
+               // buttonClickTrue()
                 Toast.makeText(requireContext(), "Failed: ${e.message}", Toast.LENGTH_SHORT).show()
             }
         }
@@ -375,10 +256,6 @@ class AddWallpaperFragment : Fragment(R.layout.fragment_add_wallpaper) {
         }
     }
 
-    private fun buttonClickTrue(){
-        binding.progressBar.visibility = View.GONE
-        binding.btnSendWallpaper.text = getString(R.string.send_content_)
-        binding.btnSendWallpaper.isEnabled = true
-    }
+
 
 }
--- app/src/main/java/com/example/setwallpaper/MainActivity.kt
+++ app/src/main/java/com/example/setwallpaper/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager.beginTransaction().replace(R.id.containerFg, MainFragment())
+        supportFragmentManager.beginTransaction().replace(R.id.containerFg, AddWallpaperFragment())
             .commit()
 
     }
